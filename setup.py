fd16c152893c12633907603fb7440eeab4c15606
kavishka-CodXlab <kavishkathilakarathna0@gmail.com>
2025-03-24 03:34:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Kavishka_Thilakarathna',
+    author_email= 'info.kavishkathilakarathna@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
